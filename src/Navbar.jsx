ecbcd938350cef4d95a6ad81e9d227001bd8000b
Shameer Meerambigari <shameeritsme21@gmail.com>
2026-03-20 23:47:59 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Navbar.css";
 
-export default function Navbar() {
+
+export default function Navbar() 
+{
   return (
     <nav className="navbar">
       <h1 className="logo">India Population Dashboard</h1>
